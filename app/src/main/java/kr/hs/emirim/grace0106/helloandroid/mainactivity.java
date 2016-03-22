52acc30e2a08bfa6863a684c3480dc713c574238
HyeonJu Jeong <0106grace@naver.com>
2016-03-22 20:49:53 +0900

    #1 Import project

---
+++ app/src/main/java/kr/hs/emirim/grace0106/helloandroid/mainactivity.java
@@ -0,0 +1,13 @@
+package kr.hs.emirim.grace0106.helloandroid;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class mainactivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainactivity);
+    }
+}
